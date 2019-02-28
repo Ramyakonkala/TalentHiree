a6cd525a004d002844a14e75c44d69b0038f1157
GGKTECH\ramya.konkala <ramya.konkala@ggktech.com>
2019-02-28 21:39:48 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,  
+    RouterModule.forRoot([]),  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
